08bf43bf1050171ec4d849aced5352674c748e9f
P.R. Neider-Ball <phillrballn@icloud.com>
2025-09-06 21:37:40 -0500

    Update TodoCounter.js

--- components/TodoCounter.js
+++ components/TodoCounter.js
@@ -2,21 +2,15 @@ class TodoCounter {
   constructor(initialTodos, containerSelector) {
     this._element = document.querySelector(containerSelector);
     this._total = initialTodos.length;
-    this._completed = 1;
+    this._completed = initialTodos.filter((todo) => todo.completed).length;
     this._updateText();
   }
 
   updateCompleted(isCompleted) {
-    if (isCompleted) {
-      this._completed += 1;
-    } else {
-      if (this._completed > 0) {
-        this._completed -= 1;
-      }
-    }
+    const delta = isCompleted ? 1 : -1;
+    this._completed = Math.max(0, this._completed + delta);
     this._updateText();
   }
-
   updateTotal(isAdding) {
     if (isAdding) {
       this._total += 1;
